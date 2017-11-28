e8ef01584731fd0c87c1f54e85f8b486eeb4d7fe
Huang Wei <wei.huang@hypereal.com>
2017-11-28 23:30:15 +0800

    Use gravity to controll zoom

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -10,8 +10,18 @@ using System.Runtime.InteropServices;
 public class CameraScript : MonoBehaviour {
 	public WebCamTexture mCamera = null;
 
+	private float gyroLimit = 0.5f;
+	private float minZ = 2.4f;
+	private float maxZ = 8;
+	private float speed = 2.0f / 60;
+
+	private int cnt = 0;
+
 	// Use this for initialization
 	void Start () {
+		// Not work at all.
+//		UnityEngine.XR.InputTracking.disablePositionalTracking = true;
+
 		mCamera = new WebCamTexture (1920, 1080, 60);
 		
 		GetComponent<Renderer>().material.mainTexture = mCamera;
@@ -21,6 +31,18 @@ public class CameraScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		var gravity = Input.gyro.gravity;
 
+		if (gravity.x < -gyroLimit) {
+			float newz = transform.position.z - speed;
+			if (newz > minZ) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, newz);
+			}
+		} else if (gravity.x > gyroLimit) {
+			float newz = transform.position.z + speed;
+			if (newz < maxZ) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, newz);
+			}
+		}
 	}
 }

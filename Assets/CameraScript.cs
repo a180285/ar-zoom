914238c37191da06dc3ffef1534238eeba037a16
Huang Wei <wei.huang@hypereal.com>
2017-11-28 21:47:48 +0800

    First commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+using System;
+using System.Runtime.InteropServices;
+
+
+public class CameraScript : MonoBehaviour {
+	public WebCamTexture mCamera = null;
+
+	// Use this for initialization
+	void Start () {
+		mCamera = new WebCamTexture (1920, 1080, 60);
+		
+		GetComponent<Renderer>().material.mainTexture = mCamera;
+		mCamera.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
